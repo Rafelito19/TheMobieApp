0b86be1ee270987cd416538c5b2ef8f020fead8c
Rafael <santana.rafael27@gmail.com>
2019-03-23 19:19:30 -0400

    Initial Commit

---
+++ TheMobieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMobieApp
+//
+//  Created by Rafael Santana on 3/23/19.
+//  Copyright © 2019 Rafael Santana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
